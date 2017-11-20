060c4a99e06a52086fc8f98bb8c98d1800be06d5
Kiko Seijo <kiko@sunnyface.com>
2017-11-20 17:32:15 +0100

    Fix.

--- src/config/repoist.php
+++ src/config/repoist.php
@@ -11,7 +11,7 @@ return [
     ],
 
     /**
-     * Paths will be used with the `app()->basePath().'/'.` function to reach app directory.
+     * Paths will be used with the `app()->basePath()` function to reach app directory.
      */
     'paths' => [
         'contracts'    => 'Repositories/Contracts/',
